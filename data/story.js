7465d349f5476e80efec3779e4b12fd2aa93ec37
kpakala4 <kpakala18@gmail.com>
2024-07-18 14:33:40 -0500

    Add files via upload

---
+++ data/story.js
@@ -0,0 +1,236 @@
+var story = {
+ "docName": "Supertutor",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "Supertutor",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "3:646",
+   "groupIndex": 0,
+   "title": "Landing page",
+   "image": "landing-page.png",
+   "index": 0,
+   "width": 1470,
+   "height": 3007,
+   "x": -1511,
+   "y": -79,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle",
+     "rect": {
+      "x": 97,
+      "y": 421,
+      "width": 197,
+      "height": 77
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "About Us",
+     "rect": {
+      "x": 993,
+      "y": 40,
+      "width": 103,
+      "height": 30
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "79:235",
+   "groupIndex": 0,
+   "title": "Login Page",
+   "image": "login-page.png",
+   "index": 1,
+   "width": 1470,
+   "height": 3007,
+   "x": 42,
+   "y": -79,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "About Us",
+     "rect": {
+      "x": 993,
+      "y": 40,
+      "width": 103,
+      "height": 30
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "supertutorlogo 3",
+     "rect": {
+      "x": 94,
+      "y": 13,
+      "width": 77,
+      "height": 77
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "28:824",
+   "groupIndex": 0,
+   "title": "About Us Page",
+   "image": "about-us-page.png",
+   "index": 2,
+   "width": 1470,
+   "height": 2274,
+   "x": 1678,
+   "y": -79,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "supertutorlogo 3",
+     "rect": {
+      "x": 94,
+      "y": 13,
+      "width": 77,
+      "height": 77
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "61:211",
+   "groupIndex": 0,
+   "title": "IMAGE",
+   "image": "image.png",
+   "index": 3,
+   "width": 30,
+   "height": 30,
+   "x": 2469,
+   "y": 2511,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "61:212",
+   "groupIndex": 0,
+   "title": "IMAGE(1)",
+   "image": "image(1).png",
+   "index": 4,
+   "width": 30,
+   "height": 21,
+   "x": 2509,
+   "y": 2516,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "61:213",
+   "groupIndex": 0,
+   "title": "IMAGE(2)",
+   "image": "image(2).png",
+   "index": 5,
+   "width": 30,
+   "height": 30,
+   "x": 2549,
+   "y": 2511,
+   "isFrame": false,
+   "type": "item",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 6,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#F5F5F5"
+  }
+ ]
+}
